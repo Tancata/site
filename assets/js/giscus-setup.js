3c14afb5bacde3277b9dd279b003f3be6e90f61f
Tancata <Tancata@users.noreply.github.com>
2026-03-03 15:21:44 +0000

    Deploying to gh-pages from @ Tancata/tancata.github.io@e6dad344d23d3539efce0d06f696686da45c69c6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Tancata/site",
+    "data-repo": "Tancata/tancata.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
