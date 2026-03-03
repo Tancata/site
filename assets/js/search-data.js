d0dfdeca0545cfe42815fb43034f01f30c439382
Tancata <Tancata@users.noreply.github.com>
2026-03-03 16:36:28 +0000

    Deploying to gh-pages from @ Tancata/tancata.github.io@7b6dd50b986189900310de6aa8de1fa3585d2e4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-people",
+          title: "people",
+          description: "lab members",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "lab publications",
           section: "Navigation",
@@ -22,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "lab members",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
